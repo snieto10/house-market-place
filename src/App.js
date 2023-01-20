3b3f6a56370e39c3c505f76128a9f6c523ae2e0f
Sergio Nieto <snieto10@gmail.com>
2023-01-19 19:06:03 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
-    <div className="App">
-      <h1>MY APP</h1>
-    </div>
+    <>
+      <Router>
+        <Route path="/" element={<Explore />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Router>
+      {/*NavBar*/}
+    </>
   );
 }
 
